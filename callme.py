e2fd3dee74cebf6c22a487b8f7fc9640bda862ee
TitaniumT <taeminkim13@gmail.com>
2013-10-13 12:52:51 -0400

    added phone number input

--- callme.py
+++ callme.py
@@ -2,18 +2,20 @@
 from twilio.rest import TwilioRestClient
 
 def call():
+	rec = raw_input("Your number: ")
+
 	# Your Account Sid and Auth Token from twilio.com/user/account
 	account_sid = "ACd04a3d6d646809d8e2aafa609e03d38d"
 	auth_token = "dae02b5d7c0b54b3fae7aa05560c1d81"
 	client = TwilioRestClient(account_sid, auth_token)
 
 	call = client.calls.create(url="http://tmkim.github.io/workplz",
-		to="+17327964267",
+		to=rec,
 		from_="+13472526162")
 
 def main():
 	call()
-	print "note: this demo won't start for 36 seconds.. just because that's how long day9 decided to wait before putting on pre-game music"
+	print "note: this demo won't start for 36 seconds.. just because that's how long day9      decided to wait before putting on pre-game music"
 
 if __name__ == '__main__':
 	main()
